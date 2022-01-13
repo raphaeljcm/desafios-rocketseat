22c357320af041fac8bdb76db3deed64f14f8fd2
Raphael Marques <raphaeljcmarques@gmail.com>
2022-01-13 14:48:08 -0300

    Finishing api-github

--- Portfolio/scripts/github-api.js
+++ Portfolio/scripts/github-api.js
@@ -1,37 +1,62 @@
+const colors = {
+  html: "#E34C26",
+  css: "#563D7C",
+  java: "#B07219",
+  typescript: "#2B7489",
+  javascript: "#F1E05A",
+  csharp: "#178600",
+  sass: "#C6538C",
+  other: "#FFF"
+}
+
 function getGitHubApi() {
   const url = `https://api.github.com/users/faeelcardoso/repos`;
 
   fetch(url).then(res => res.json()).then(datas => {
 
     datas.forEach(data => {
+
       //Creating all the structure, find a way to do it fast
-
-      console.log(data.name);
-      console.log(data.html_url);
-      console.log(data.description);
-      console.log(data.stargazers_count);
-      console.log(data.forks_count);
-      console.log(data.language);
-    });
-
-    /*
-      <div class="section-projects-each-project">
-            <a href="#"><img src="./assets/folder.svg" alt="Folder icon"> my-onix</a>
-            <p>Consulte os códigos de erro que aparecem no painel do veículo Onix.</p>
-            <div class="section-projects-each-project-icons">
-              <div>
-                <img src="./assets/star.svg" alt="Star Icon">
-                <span>100</span>
-                <img src="./assets/git-branch.svg" alt="Git branch icon">
-                <span>100</span>
-              </div>
-              <div class="section-projects-each-project-icons-tech">
-                <div class="circle"></div>
-                <span>Javascript</span>
-              </div>
+      insert.innerHTML += `
+        <div class="section-projects-each-project">
+          <a href="${data.html_url}" target="_blank"><img src="./assets/folder.svg" alt="Folder icon">${data.name}</a>
+          <p>${data.description == null ? 'Sem descrição' : data.description}</p>
+          <div class="section-projects-each-project-icons">
+            <div>
+              <img src="./assets/star.svg" alt="Star Icon">
+              <span>${data.stargazers_count}</span>
+              <img src="./assets/git-branch.svg" alt="Git branch icon">
+              <span>${data.forks_count}</span>
             </div>
-    */
+            <div class="section-projects-each-project-icons-tech">
+              <div class="circle"></div>
+              <span>${data.language == null ? 'Other' : data.language}</span>
+            </div>
+          </div>
+        </div>
+      `;
+      
+      // Changing the color of the circle 
+      if(data.language === null) {
+        changeColor("other")
+      } else if(data.language === "C#") {
+        changeColor("csharp")
+      } else {
+        changeColor(data.language.toLowerCase());
+      }
+    });
   });
+}
+
+function changeColor(language) {
+  const currentElement = document.getElementById("insert").lastElementChild;
+  const currentCircle = currentElement.querySelectorAll(".circle")[0];
+
+  for(const color in colors) {
+    if(language === color) {
+      currentCircle.style.backgroundColor = colors[color];
+    } 
+  }
 }
 
 function init() {
