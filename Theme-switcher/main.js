314deb4e12ea53a562facdbc7d24c93f896e106f
Raphael Marques <raphaeljcmarques@gmail.com>
2022-02-27 12:53:48 -0300

    theme switcher

--- Theme-switcher/main.js
+++ Theme-switcher/main.js
@@ -1,25 +1,5 @@
 function timeSwitcher() {
   const ball = document.getElementById('ball');
-  const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-  // to know if the user has set his theme to dark on browser
-  if(prefersDarkScheme.matches) {
-    document.body.style.backgroundColor = '#292C35';
-    ball.classList.add('moon');
-  } else {
-    document.body.style.backgroundColor = '#F1F1F1';
-  }
-
-  // to know if the user has set his theme to dark on android
-  switch(getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
-    case Configuration.UI_MODE_NIGHT_YES:
-      document.body.style.backgroundColor = '#292C35';
-      ball.classList.add('moon');
-      break;
-    case Configuration.UI_MODE_NIGHT_NO:
-      document.body.style.backgroundColor = '#F1F1F1';
-      break;
-  }
 
   ball.addEventListener('click', () => {
     ball.classList.toggle('moon');
@@ -32,9 +12,6 @@ function timeSwitcher() {
       document.body.style.backgroundColor = '#F1F1F1';
     }
   });
-}
-
-
-
+} 
 
 timeSwitcher();
